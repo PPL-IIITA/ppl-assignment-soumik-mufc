cd34d4c542f634827033b4dc821e3376c6adb3ac
Soumik Chatterjee <soumik53k@gmail.com>
2017-02-27 01:28:24 +0530

    UML diagram added. Minor changes made.

--- Helper.h
+++ Helper.h
@@ -11,7 +11,6 @@ class Helper
 {
 public:
 	friend void scan_data(std::vector<miser::Miser> &vb1, std::vector<generous::Generous> &vb2, std::vector<geeky::Geeky> &vb3, std::vector<choosy::Choosy> &vg1, std::vector<normal::Normal> &vg2, std::vector<desperate::Desperate> &vg3);
-	friend void display(const std::vector<miser::Miser> &vb1, const std::vector<generous::Generous> &vb2, const std::vector<geeky::Geeky> &vb3, const std::vector<choosy::Choosy> &vg1, const std::vector<normal::Normal> &vg2,const std::vector<desperate::Desperate> &vg3);
 	friend void make_couple(std::vector<miser::Miser> &vb1, std::vector<generous::Generous> &vb2, std::vector<geeky::Geeky> &vb3, std::vector<choosy::Choosy> &vg1, std::vector<normal::Normal> &vg2, std::vector<desperate::Desperate> &vg3, std::vector <std::pair<std::string, std::string>> &couples);
 	friend int maximum( int a, int b, int c );
 	friend void store_couples(std::vector <std::pair<std::string, std::string>> &couples);
@@ -19,8 +18,6 @@ public:
 };
 
 void scan_data(std::vector<miser::Miser> &vb1, std::vector<generous::Generous> &vb2, std::vector<geeky::Geeky> &vb3, std::vector<choosy::Choosy> &vg1, std::vector<normal::Normal> &vg2, std::vector<desperate::Desperate> &vg3);
-
-void display(const std::vector<miser::Miser> &vb1, const std::vector<generous::Generous> &vb2, const std::vector<geeky::Geeky> &vb3, const std::vector<choosy::Choosy> &vg1, const std::vector<normal::Normal> &vg2,const std::vector<desperate::Desperate> &vg3);
 
 void make_couple(std::vector<miser::Miser> &vb1, std::vector<generous::Generous> &vb2, std::vector<geeky::Geeky> &vb3, std::vector<choosy::Choosy> &vg1, std::vector<normal::Normal> &vg2, std::vector<desperate::Desperate> &vg3, std::vector <std::pair<std::string, std::string>> &couples);
 
